452bd8f93f5bc4eb4730d405c8046310c059b27d
Jett Walley <ben.walley@classyllama.com>
2021-02-18 13:31:16 -0600

    make mine get larger when needed

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ function init() {
     window.requestAnimationFrame(initGameLoop);
     initMine();
     updateDisplayedQty();
+    controlStoreVisibility();
+    addStoreListeners();
 }
 
 function initGlobalVariables() {
@@ -22,7 +24,7 @@ function initGlobalVariables() {
             iron: {probability: 10, color: '#ed8f1c', hardness: 2500, value: 10},
             diamond: {probability: 1, color: '#9cc2ff', hardness: 5000, value: 50}
             },
-        playerData: {inventory: {gas: 500}, x: 50, y:-1, speed: 1, color: "#2988a0", playerDrawX: Math.floor(document.getElementById("mainGameCanvas").width/2), playerDrawY: Math.floor(document.getElementById("mainGameCanvas").height/2)},
+        playerData: {inventory: {gas: 5000, money: 100}, x: 50, y:-1, speed: 10, color: "#2988a0", playerDrawX: Math.floor(document.getElementById("mainGameCanvas").width/2), playerDrawY: Math.floor(document.getElementById("mainGameCanvas").height/2)},
         playerMoving: {started: 0, direction: undefined, currentMove: undefined},
         mineData: {blockWidth: 100},
         groundHeight: 300,
@@ -31,8 +33,39 @@ function initGlobalVariables() {
     // TODO: save game data to the local storage;
 }
 
-function initMine() {
-    let mineArray = [];
+function addStoreListeners() {
+    let storeItems = document.querySelectorAll(".store .buy-item");
+    for (let i = 0; i < storeItems.length; i++) {
+        storeItems[i].querySelector(".buy-item-add").addEventListener("click", function(e) {
+            let element = e.target.parentElement.dataset;
+            gameData.playerData.inventory[element.item] ? gameData.playerData.inventory[element.item] += parseInt(element.qty) : gameData.playerData.inventory[element.item] = parseInt(element.qty)
+            gameData.playerData.inventory.money -= parseInt(element.price);
+            updateDisplayedQty();
+            controlStoreVisibility();
+        })
+    }
+}
+
+function controlStoreVisibility() {
+    const isPlayerAboveGround = gameData.playerData.y === -1;
+    const playersMoney = gameData.playerData.inventory.money;
+    let storeItems = document.querySelectorAll(".store .buy-item");
+
+    if (isPlayerAboveGround) {
+        // check whether they have enough money to buy stuff;
+        for (let i = 0; i < storeItems.length; i++) {
+            let item = storeItems[i].dataset.item;
+            let price = storeItems[i].dataset.price;
+            if (playersMoney < price) {
+                storeItems[i].classList.add("disabled");
+            } else if (playersMoney >= price) {
+                storeItems[i].classList.remove("disabled");
+            }
+        }
+    }
+}
+
+function getRandomBlock() {
     let blockTypeArray = [];
     let averageFrom = 0;
     // get array of block types
@@ -40,21 +73,28 @@ function initMine() {
         blockTypeArray.push(block);
         averageFrom += gameData.blockTypeMap[block].probability;
     }
+
+    let randValue = Math.floor(Math.random() * averageFrom);
+    let counter = 0;
+    let blockType = 'stone';
+    for(let j = 0; j < blockTypeArray.length; j++) {
+        counter += gameData.blockTypeMap[blockTypeArray[j]].probability;
+        if (randValue < counter) {
+            return blockTypeArray[j];
+        }
+    }
+
+    return blockType;
+}
+
+function initMine() {
+    let mineArray = [];
+
     for(let i = 0; i < window.gameData.mineSize.width; i++) {
         let mineRow = [];
         for(let q = 0; q < window.gameData.mineSize.height; q++) {
-            let randValue = Math.floor(Math.random() * averageFrom);
-            let counter = 0;
-            let blockType = 'stone';
-            for(let j = 0; j < blockTypeArray.length; j++) {
-                counter += gameData.blockTypeMap[blockTypeArray[j]].probability;
-                if (randValue < counter) {
-                    blockType = blockTypeArray[j];
-                    break;
-                }
-            }
 
-            mineRow.push({blockType: blockType, isMined: false});
+            mineRow.push({blockType: getRandomBlock(), isMined: false});
         }
         mineArray.push(mineRow);
     }
@@ -107,6 +147,18 @@ function movePlayer(x, y) {
         potentialPosition.y = -1; // prevent them from moving up above -1
         window.gameData.playerData.x = potentialPosition.x;
         window.gameData.playerData.y = potentialPosition.y;
+        // if they have items, cash them in
+        let inventory = gameData.playerData.inventory;
+        for(const item in inventory) {
+            // check if it exists in the mapping table
+            if(gameData.blockTypeMap[item] !== undefined) {
+                // cash it in;
+                gameData.playerData.inventory.money += gameData.blockTypeMap[item].value * inventory[item];
+                gameData.playerData.inventory[item] = 0;
+                updateDisplayedQty();
+                controlStoreVisibility();
+            }
+        }
     } else {
         const blockType = window.gameData.mine[potentialPosition.y][potentialPosition.x].blockType;
         const gasAfterMove = gameData.playerData.inventory.gas - gameData.blockTypeMap[blockType].hardness / 100;
@@ -135,6 +187,7 @@ function updateDisplayedQty() {
     const coalDisplay = document.querySelector(".navbar .coal .inventory-qty");
     const ironDisplay = document.querySelector(".navbar .iron .inventory-qty");
     const diamondDisplay = document.querySelector(".navbar .diamond .inventory-qty");
+    const moneyDisplay = document.querySelector(".navbar .money .inventory-qty");
 
     gasDisplay.innerHTML = gameData.playerData.inventory.gas ?? 0;
     dirtDisplay.innerHTML = gameData.playerData.inventory.dirt ?? 0;
@@ -142,6 +195,7 @@ function updateDisplayedQty() {
     coalDisplay.innerHTML = gameData.playerData.inventory.coal ?? 0;
     ironDisplay.innerHTML = gameData.playerData.inventory.iron ?? 0;
     diamondDisplay.innerHTML = gameData.playerData.inventory.diamond ?? 0;
+    moneyDisplay.innerHTML = gameData.playerData.inventory.money ?? 0;
 }
 
 function resizeCanvas(canvas) {
@@ -192,7 +246,7 @@ function calculatePlayerMove() {
             // they're moving underground
             let nextBlock = gameData.blockTypeMap[gameData.mine[nextBlockY][nextBlockX].blockType];
             // check if they've been moving long enough
-            if(Date.now() - moveStartTime >= nextBlock.hardness) {
+            if(Date.now() - moveStartTime >= nextBlock.hardness / gameData.playerData.speed) {
                 movePlayer(window.gameData.playerMoving.direction.x, window.gameData.playerMoving.direction.y);
                 window.gameData.playerMoving.started = Date.now();
                 // TODO: add a loader to show how far they've mined into that block. Eventually I should actually move them little bits.
@@ -208,20 +262,59 @@ function drawMine() {
     const numberHorizontal = Math.ceil((window.gameData.canvas.width/window.gameData.mineData.blockWidth) + 3);
     const numberVertical = Math.ceil((window.gameData.canvas.height/window.gameData.mineData.blockWidth) + 3);
     const blockWidth = window.gameData.mineData.blockWidth;
-    const playerPosX = window.gameData.playerData.x
-    const playerPosY = window.gameData.playerData.y
-    const playerBlockX = window.gameData.playerData.playerDrawX - (blockWidth/2);
-    const playerBlockY = window.gameData.playerData.playerDrawY - (blockWidth/2);
-    const numberBeforePlayer = Math.floor(numberHorizontal / 2)
-    const numberAbovePlayer = Math.floor(numberVertical / 2)
-    const leftPosition = playerPosX - numberBeforePlayer;
-    const topPosition = playerPosY - numberAbovePlayer;
-    const firstDrawX = playerBlockX - (numberBeforePlayer * blockWidth)
-    const firstDrawY = playerBlockY - (numberAbovePlayer * blockWidth)
+    let playerPosX = window.gameData.playerData.x
+    let playerPosY = window.gameData.playerData.y
+    let playerBlockX = window.gameData.playerData.playerDrawX - (blockWidth/2);
+    let playerBlockY = window.gameData.playerData.playerDrawY - (blockWidth/2);
+    let numberBeforePlayer = Math.floor(numberHorizontal / 2)
+    let numberAbovePlayer = Math.floor(numberVertical / 2)
+    let leftPosition = playerPosX - numberBeforePlayer;
+    let topPosition = playerPosY - numberAbovePlayer;
+    let firstDrawX = playerBlockX - (numberBeforePlayer * blockWidth)
+    let firstDrawY = playerBlockY - (numberAbovePlayer * blockWidth)
+    const numberToAddToMine = 20;
 
-    // if(topPosition < 0) {
-    //     // pass
-    // }
+    // check if you're too far to one side
+    // if so, either stop, or add to the array
+    if(playerPosX + numberBeforePlayer > gameData.mineSize.width) {
+        // add to end of mine
+        for (var i = 0; i < gameData.mineSize.height; i++) {
+            for (var q = 0; q < numberToAddToMine; q++) {
+                gameData.mine[i].push({blockType: getRandomBlock(), isMined: false});
+            }
+        }
+        gameData.mineSize.width += numberToAddToMine;
+    } else if (leftPosition < 0) {
+        // add to beginning of mine
+        for (var i = 0; i < gameData.mineSize.height; i++) {
+            for (var q = 0; q < numberToAddToMine; q++) {
+                gameData.mine[i].unshift({blockType: getRandomBlock(), isMined: false});
+            }
+        }
+        gameData.mineSize.width += numberToAddToMine;
+        // move current position
+        gameData.playerData.x += numberToAddToMine;
+        // recalculate variables
+        playerPosX = window.gameData.playerData.x
+        playerPosY = window.gameData.playerData.y
+        playerBlockX = window.gameData.playerData.playerDrawX - (blockWidth/2);
+        playerBlockY = window.gameData.playerData.playerDrawY - (blockWidth/2);
+        numberBeforePlayer = Math.floor(numberHorizontal / 2)
+        numberAbovePlayer = Math.floor(numberVertical / 2)
+        leftPosition = playerPosX - numberBeforePlayer;
+        topPosition = playerPosY - numberAbovePlayer;
+        firstDrawX = playerBlockX - (numberBeforePlayer * blockWidth)
+        firstDrawY = playerBlockY - (numberAbovePlayer * blockWidth)
+    } else if (playerPosY + numberAbovePlayer >= gameData.mineSize.height) {
+        for(let i = 0; i < numberToAddToMine; i++) {
+            let mineRow = [];
+            for(let q = 0; q < gameData.mineSize.width; q++) {
+                mineRow.push({blockType: getRandomBlock(), isMined: false});
+            }
+            gameData.mine.push(mineRow);
+        }
+        gameData.mineSize.height += numberToAddToMine;
+    }
 
     for(let i = 0; i < numberVertical; i++) {
         for(let q = 0; q < numberHorizontal; q++) {
